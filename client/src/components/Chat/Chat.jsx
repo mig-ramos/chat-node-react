9da69e907894796235c5eb50eba824787a894e08
Miguel Arcanjo <mig.ramos10@gmail.com>
2025-01-14 15:39:26 -0300

    limpa projeto vite e cria components chat e join

---
+++ client/src/components/Chat/Chat.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Chat() {
+  return (
+    <div>
+      <h1>Chat</h1>
+      <input type="text" placeholder="Mensagem" />
+      <button>Mensagem</button>
+    </div>
+  );
+}
+
+export default Chat;
---
+++ client/src/components/Join/Join.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Join() {
+  return (
+    <div>
+      <h1>Join</h1>
+      <input type="text" placeholder="Nome de usuário" />
+      <button>Entrar</button>
+    </div>
+  );
+}
+
+export default Join;
